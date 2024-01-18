0c443f9db44bf571a14b24e79c0bf0fa12be87cf
GarrettBoag <146123631+GarrettBoag@users.noreply.github.com>
2024-01-18 13:40:58 -0500

    my information v1

---
+++ myInfo.py
@@ -0,0 +1,12 @@
+# CS175L
+#Garett Boag
+#myInfo
+
+print("Hi, I'm Garrett Boag")
+
+#fake address
+print("I live at 2332 Oak Drive in Westminster Maryland," +
+      "and my zip is 21049")
+
+#fake number
+print("If you need to reach out to me, my phone number is 441-330-2211")
