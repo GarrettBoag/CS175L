c49d8b3db56eaf4a680627deaee85e406d29ddf1
GarrettBoag <146123631+GarrettBoag@users.noreply.github.com>
2024-03-21 08:42:02 -0400

    Add files via upload

---
+++ AverageFromInput.py
@@ -0,0 +1,25 @@
+#Garrett Boag
+#3/21/24
+#CS 175L 01
+
+
+def main():
+
+    total = 0.0
+    rep = 0
+    num = 0
+    
+    infile = open("numbers.txt","r")
+    for line in infile:
+
+        num = line.strip()
+        num = int(num)
+        #print(num)
+        
+        rep += 1
+        total += num
+        
+        print(f'I read in {rep} number(s) Current number is:  {num} Total is: {total}')
+    print(f'Average: {total / 3}')
+    #print(total)
+main()
